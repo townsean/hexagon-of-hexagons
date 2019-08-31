0c55c6aa53213dc02b1d204fe85a62387048409e
Ashley Grenon <ashley.grenon@gmail.com>
2019-08-31 10:16:40 -0500

    more boilerplate code

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 /**
+ * Event handler for the window load. Trigger first
+ * generate so the user isn't greeted with blank data
+ */
+window.addEventListener('load', () => {
+    generateHexagon();
+    convertSvgToText();
+});
+
+/**
  * Event handler for the generate button. On click draw
  * a hexagon of hexagons based off the given user parameters.
  */
 document.getElementById('generate').onclick = () => {
-    let svg = document.querySelector('svg');
-    let length = document.getElementById('side-length').value;
-
-    generateHexagon(length);
-
-    let serializer = new XMLSerializer();
-    let source = serializer.serializeToString(svg);
-
-    document.querySelector('textarea').innerHTML = source;
+    generateHexagon();
+    convertSvgToText();
 }
 
 /**
  * Event handler for the copy button. On click select
  * the text contents of the text area and copy it to the clipboard
  */
-document.getElementById('copy').onclick = () => {
+document.getElementById('copy').onclick = () => {    
     let textarea = document.querySelector('textarea');
     textarea.select();
     document.execCommand("copy");
@@ -27,6 +29,19 @@ document.getElementById('copy').onclick = () => {
 /**
  * Draw a hexagon of hexagons in SVG
  */
-generateHexagon = (numberOfSides) => {
-    console.log(numberOfSides);
+generateHexagon = () => {    
+    let svg = document.querySelector('svg');
+    let length = document.getElementById('side-length').value;
+    console.log(length);
+}
+
+/**
+ * Serialize the SVG and display the text 
+ */
+convertSvgToText = () => {
+    let svg = document.querySelector('svg');
+    let serializer = new XMLSerializer();
+    let source = serializer.serializeToString(svg);
+
+    document.querySelector('textarea').innerHTML = source;
 }
